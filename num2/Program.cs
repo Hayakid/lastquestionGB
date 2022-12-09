77f9c07b695c72cb86fa4f7496f09521259f728f
Danil <hisdan56708@gmail.com>
2022-12-09 15:11:41 +0300

    lastOne

---
+++ num2/Program.cs
@@ -0,0 +1,13 @@
+﻿//Задайте значения M и N. Напишите программу, которая найдёт сумму
+// натуральных элементов в промежутке от M до N.
+int n, m, s=0;
+Console.Write("Введи значение от: ");
+n = Convert.ToInt16(Console.ReadLine());
+Console.Write("Введи значение до: ");
+m = Convert.ToInt16(Console.ReadLine());
+for (int i = m; i >= n; i--)
+{
+    s += i;
+}
+Console.Write("Сумма чисел начиная от n до m =  "  );
+Console.WriteLine(s);
---
+++ num3/Program.cs
@@ -0,0 +1,16 @@
+﻿//пониамю не совсем правильно сделал
+static uint Akk(int n, uint m)
+{
+    if (n == 0)
+        return m + 1;
+    else
+      if ((n != 0) && (m == 0))
+        return Akk(n - 1, 1);
+    else
+        return Akk(n - 1, Akk(n, m - 1));
+}
+ 
+Console.WriteLine(Akk(2, 4));
+Console.WriteLine(Akk(0, 0)); 
+Console.WriteLine(Akk(6, 5)); 
+Console.ReadKey();
